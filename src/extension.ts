1020457177a8a01169abb9044f752ec68e51af4a
Moshe Plotkin <plotkinm@newpaltz.edu>
2024-11-03 23:47:37 -0500

    seemed to fix issues

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 import * as vscode from 'vscode';
 import simpleGit from 'simple-git';
-import fetch from 'node-fetch';
+import { parse } from 'path';
+//import fetch from 'node-fetch';
 
 export function activate(context: vscode.ExtensionContext) {
   let disposable = vscode.commands.registerCommand('extension.createGithubIssue', async () => {
@@ -14,7 +15,7 @@ export function activate(context: vscode.ExtensionContext) {
     const line = selection.active.line + 1;
     const filePath = document.uri.path;
 
-    const repoUrl = await getRepoUrl();
+    const repoUrl = await getRepoUrl(filePath);
     const issueUrl = constructIssueUrl(repoUrl, filePath, line);
 
     vscode.env.openExternal(vscode.Uri.parse(issueUrl));
@@ -23,24 +24,31 @@ export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(disposable);
 }
 
-async function getRepoUrl(): Promise<string> {
-  const git = simpleGit();
-  const remotes = await git.getRemotes(true);
-  const originRemote = remotes.find(remote => remote.name === 'origin');
-  const repoUrl = originRemote ? originRemote.refs.fetch : 'https://github.com/your-username/your-repo'; // Fallback URL
+async function getRepoUrl(filePath: string): Promise<string> {
 
-  if (originRemote) {
-    const repoPath = repoUrl.replace('https://github.com/', '');
-    const apiUrl = `https://api.github.com/repos/${repoPath}`;
-    const response = await fetch(apiUrl);
-    const repoData = await response.json() as { fork: boolean, parent: { html_url: string } };
+    try {
+        const path = parse(filePath);
+        const git = simpleGit(path.dir.substring(1)); // Remove leading slash
+        const remotes = await git.getRemotes(true);
+        const originRemote = remotes.find(remote => remote.name === 'origin');
+        const repoUrl = originRemote ? originRemote.refs.fetch : 'https://github.com/your-username/your-repo'; // Fallback URL
 
-    if (repoData.fork && repoData.parent) {
-      return repoData.parent.html_url;
+        if (originRemote) {
+            const repoPath = repoUrl.replace('https://github.com/', '');
+            const apiUrl = `https://api.github.com/repos/${repoPath}`;
+            const response = await fetch(apiUrl);
+            const repoData = await response.json() as { fork: boolean, parent: { html_url: string } };
+
+            if (repoData.fork && repoData.parent) {
+            return repoData.parent.html_url;
+            }
+        }
+
+        return repoUrl;
+    }catch(e){
+        console.error(e);
     }
-  }
-
-  return repoUrl;
+    return ''
 }
 
 function constructIssueUrl(repoUrl: string, filePath: string, line: number): string {
